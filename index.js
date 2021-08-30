46d69d1cf875465123de2f329d38911353da4789
Kenneth Landis <Eightfletcher@msn.com>
2021-08-30 05:41:31 -0500

    finished my results
    finished

--- index.js
+++ index.js
@@ -44,6 +44,63 @@ console.log(powerCalc(10, 3))
 console.log(powerCalc(2, 3))
 console.log(powerCalc(3, 0))
 
+// Reverse String
+const reverseString = function(string, i=1, result='') {
+	if (result.length === string.length) {
+  return result
+  }
+  if (i < string.length + 1) {
+  let x = string.length - i
+  i++
+  result = result + string[x]
+  return reverseString(string, i, result)
+  }
+}
+
+console.log(reverseString('dog'))
+console.log(revereString('happydays'))
+
+//nth Triangular Number
+const triangularNumber = function(n) {
+	if (n === 0) {
+  	return n
+  }
+  
+	return n = n + triangularNumber(n-1)
+}
+
+console.log(triangularNumber(4))
+
+//String Splitter
+
+const stringSplitter = function(string, splitter) {
+	if (string.indexOf(splitter) === -1) {
+  	return [string.slice(0, string.length)]   
+  }
+  return [string.slice(0, string.indexOf(splitter)), ...stringSplitter(string.slice(string.indexOf(splitter) + 1), splitter)]
+  }
+  
+
+console.log(stringSplitter('02/20/2020', '/'))
+
+//fibonacci
+
+const fibonacci = function(n) {
+	if (n < 1) {
+  	console.log('done')
+  	return 0
+  }
+  if (n <= 2) {
+  	return 1
+  }
+  return fibonacci(n-1) + fibonacci(n-2)
+}
+
+console.log(fibonacci(20))
+
+
+//maze solver//
+
 let maze = [
     [' ', ' ', ' ', '*', ' ', ' ', ' '],
     ['*', '*', ' ', '*', ' ', '*', ' '],
@@ -100,3 +157,75 @@ const mazeSolver = function(maze, row=0, col=0, directions='') {
 }
 
 console.log(mazeSolver(maze, 0, 0, ''));
+
+//refactor maze not sure
+
+let maze = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', 'e']
+];
+
+const mazeSolver = function(maze, row=0, col=0, directions='', path=[]) {
+if (row >= (maze.length) || col >= maze[0].length) {
+  return;
+}
+
+if (row < 0 || col < 0) {
+  return;
+}
+
+if (maze[row][col] === 'e') {
+  path.push(directions)
+  console.log(`this is a path: ${directions}`)
+  return path
+}
+
+if (maze[row][col] === ' ') {
+  maze[row][col] = 'v'
+  mazeSolver(maze, row, col - 1, directions + 'L'),
+  mazeSolver(maze, row, col + 1, directions + 'R'),
+  mazeSolver(maze,row - 1, col, directions + 'U'),
+  mazeSolver(maze, row + 1, col, directions + 'D')
+  maze[row][col] = ' ';
+}
+
+if (maze[row][col] === '*') {
+  return;
+  }
+}
+
+mazeSolver(maze, 0, 0, '')
+
+//anagrams
+
+
+
+const permutations = function(string1) {
+	if (string1.length < 2) {
+  	return string1
+  }
+  let perms = []
+	for (let i=0; i < string1.length; i++) {
+   	let char = string1[i]
+    let rem = string1.slice(0, i) + string1.slice(i+1, string1.length)
+    for (let permutation of permutations(rem)) {
+    	perms.push(char + permutation)
+    }
+   }
+   return perms
+}
+
+console.log(permutations('abcd'))
+
+//binary
+
+const binaryRep = int => {
+  if (int === 0) {
+    return '';
+  }
+  
+  const quotient = Math.floor(int/2)
+  const binDigit = int % 2;
+  return binDigit + binaryRep(quotient)
+}
